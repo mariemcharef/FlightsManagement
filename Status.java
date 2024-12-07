d6fadf316f3de823190fa6c21bae07abf4910f96
mariemcharef <167601146+mariemcharef@users.noreply.github.com>
2024-12-07 12:52:22 +0100

    Add files via upload

--- Status.java
+++ Status.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package AirCraft;
+package Person;
 
-/**
- *
- * @author moham
- */
-public enum Status {
-    WORKING,
-    OLD,
-    BROKEN,
-    REFACTORING,
+
+public enum  Status {
+    ACTIVE,
+    INACTIVE,
+    SUSPENDED,
+    CLOSED
 }
